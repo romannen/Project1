bf0af3935f19521ce946b5a1950d749fbac5a31c
romannen <roma0altoid@gmail.com>
2025-05-23 16:54:37 +0300

    Update import random.py

--- import random.py
+++ import random.py
@@ -15,6 +15,9 @@ else:
 random_number = random.randint(0, top_of_range)
 guesses = 0
 
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
